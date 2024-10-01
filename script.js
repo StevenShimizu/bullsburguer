48c13ad092f3e0b477a0047f6263e4d6b9608abc
Steven <shimizusteven8@gmail.com>
2024-10-01 00:32:12 -0300

    Funcionalidade de menu

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ menu.addEventListener('click', () => {
     NavMenu.classList.toggle('ativo');
 })
 
+// Funcionalidade de menu adicionada por Steven - Membro 6
+const menu = document.querySelector('.menu');
+const NavMenu = document.querySelector('.nav-menu');
 
+menu.addEventListener('click', () => {
+    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo');
+})
